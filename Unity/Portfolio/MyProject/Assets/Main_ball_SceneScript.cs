85fe6c7d482e90d6491b36c409eae3404c4ae7fc
nakaseshota <uverworld_0605@yahoo.co.jp>
2021-07-12 21:06:03 +0900

    スコア追加

--- Unity/Portfolio/MyProject/Assets/Main_ball_SceneScript.cs
+++ Unity/Portfolio/MyProject/Assets/Main_ball_SceneScript.cs
@@ -1,43 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Timers;
 
 public class Main_ball_SceneScript : MonoBehaviour
 {
     bool f = true; // キーの連続入力対策フラグ
-    Vector3 cv = new Vector3(0f, 3f, -15f); // カメラ位置用(オブジェクトに加算)
-    Rigidbody rb = null;
-    GameObject ex = null;
+    bool finish = false; // ゲーム終了フラグ
 
-    float dt = 1.0f; // 爆破開始速度
-
-    Color[] cdata = {
-        Color.white, Color.black, Color.gray,
-        Color.red, Color.green, Color.blue,
-        Color.cyan, Color.magenta, Color.yellow,
-        new Color(1f, 1f, 0f, 1f)
+    Dictionary<string, bool> flag = new Dictionary<string, bool>()
+    {
+        {"OtherSphere 0", false},
+        {"OtherSphere 1", false},
+        {"OtherSphere 2", false},
+        {"OtherSphere 3", false},
     };
-    System.Random r = new System.Random();
 
-    Dictionary<string, int> data = new Dictionary<string, int>(); // 辞書
+    // パラメーター変数
+    Dictionary<string, int> param = new Dictionary<string, int>()
+    {
+        {"power", 30}, // 体力
+        {"level", 1}, // レベル
+        {"exp", 0} // 経験値
+    };
+
+    Vector3 cv = new Vector3(0f, 3f, -15f); // カメラ位置用(オブジェクトに加算)
+    Rigidbody rb = null; // リジッドボディ
+    GameObject ex = null; // 爆発
+
+    Text message = null; // テキスト表示用UI
+
+    Text score = null; // スコア表示用UI
+    Text high = null; // ハイスコア用UI
+    int EndTime = 1000; // 開始時間
+    int Score = 0; // スコア
+    int High = 0; // ハイスコア
+
+    // float dt = 1.0f; // 爆破開始速度
+
+    // 初期化処理
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        ex = GameObject.Find("BigExplosion");
+        message = GameObject.Find("Message").GetComponent<Text>();
+        //*スコア関係の設定
+        score = GameObject.Find("Score").GetComponent<Text>();
+        high = GameObject.Find("High").GetComponent<Text>();
+        EndTime = (int)Time.time + 1000;
+        High = PlayerPrefs.GetInt("high");
+        high.text = "Record: " + High;
     }
 
-    void Update()
+    // 表示の更新
+    void FixedUpdate()
     {
+        if (Finish()){ return; } //ゲーム終了時は抜ける
+        Score = EndTime - (int)Time.time; //*スコア計算
+        score.text = "Time: " + Score; //*スコア表示
+        if (Score <= 0)
+        {   // スコアがゼロ以下なら終了
+            Loss();
+            return;
+        }
+        // カメラ位置の設定
         var sv = transform.position;
-        sv.y = 1f; // カメラ位置 Y軸固定用
-        Camera.main.transform.position = sv + cv; // カメラ位置
-
+        sv.y = 1f;
+        Camera.main.transform.position = sv + cv;
+        // 入力処理
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
-        var v = new Vector3(x, 0, y);
+        //var d = (float)(2.0 + param["level"] / 2);
+        var v = new Vector3(x * param["level"], 0, y * param["level"]);
 
+        // ジャンプ処理
         var vz = Vector3.zero;
-
         var jp = Input.GetAxis("Fire1");
 
         if (jp > 0)
@@ -52,41 +89,137 @@ public class Main_ball_SceneScript : MonoBehaviour
         {
             f = true;
         }
+        // プレイヤーを動かす
         rb.AddForce(v + vz);
     }
 
-    // ■ otherタグのボール接触時爆発
-    /* 設定
-    1. ループ、ゲーム開始時に再生のチェックを外す
-    */
-    void OnTriggerEnter(Collider collider)
+    // レベルを得る
+    public int Level()
     {
-        if (collider.gameObject.tag == "Other")
-        {
-            // 爆発
-            var go = collider.gameObject;
-            ex.transform.position = go.transform.position;
-            var main = ex.GetComponent<ParticleSystem>().main;
-            main.startSpeed = dt;
-            dt /= 2;
-            ex.GetComponent<ParticleSystem>().Play();
-            
-            var data = collider.gameObject.GetComponent<OtherData>();
-            data.AddValue();
-            data.Color = cdata[r.Next(10)];
-            ChangeOther(collider.gameObject);
+        return param["level"];
+    }
+
+    // ゲーム終了かどうか
+    public bool Finish()
+    {
+        return finish;
+    }
+
+    // 経験値を増やす
+    public void AddExp(int n)
+    {
+        param["exp"] += n;
+        if (param["exp"] >= 10)
+        {   // 10以上ならレベルアップ
+            levelUp();
         }
     }
 
-    // GameObjectの表示を更新
-    void ChangeOther(GameObject ob)
+    // 戦闘(衝突時)
+    void Fight(GameObject go)
     {
-        var data = ob.GetComponent<OtherData>();
-        var rd = ob.GetComponent<Renderer>();
-        var d = 1.0f - data.value * 0.1f;
-        var c = data.Color;
-        c.a = d;
-        rd.material.color = c;
-        rd.material.SetFloat("_Metallic", d);
+        var er = go.GetComponent<Rigidbody>();
+        var pr = GetComponent<Rigidbody>();
+        var od = er.GetComponent<OtherData>();
+        // magnitudeを比べて勝ちを判定
+        if (er.velocity.magnitude > pr.velocity.magnitude)
+        {   // 負けた時の処理
+            Debug.Log("Player: Loss...");
+            param["power"] -= od.Level();
+            AddExp(od.Level() / 2);
+            if (param["power"] <= 0)
+            { // 体力ゼロ以下ならゲームオーバー
+                Loss();
+            }
+        }
+        else
+        {   // 勝った時の処理
+            Debug.Log("Player: Gotha!");
+            AddExp(od.Level());
+        }
+    }
+
+    // 衝突時の処理
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Finish())
+        {
+            return; // 終了時は抜ける
+        }
+        if (collision.gameObject.tag == "Other")
+        {
+            var data = collision.gameObject.GetComponent<OtherData>();
+            Fight(collision.gameObject);
+        }
+    }
+
+    // 指定のフラグをONにする
+    public void Flag(string flg)
+    {
+        flag[flg] = true;
+        if (CheckFlag())
+        {   // フラグをチェックしtrueなら終了
+            Win();
+        }
+    }
+
+    // フラグをチェック(すべてONかどうか)
+    bool CheckFlag()
+    {
+        var f = true;
+        foreach(var item in flag)
+        {   // flagの全値をチェック
+            if (item.Value == false)
+            {
+                f = false;
+            }
+        }
+        return f; // すべてtrueならtrue
+    }
+    
+    // レベルアップ
+    void levelUp()
+    {
+        if (param["level"] == 5)
+        {
+            return; // 終了時は抜ける
+        }
+        param["level"]++; // レベルを上げる
+        param["exp"] = 0; // 経験値をゼロに
+        message.text = "Level " + param["level"]; // テキスト表示
+        TimerStart(); // タイマー開始
+    }
+
+    //タイマー開始
+    void TimerStart()
+    {
+        StartCoroutine(TimerEnd()); // TimerEndを設定
+    }
+
+    // タイマーで実行する処理
+    IEnumerator TimerEnd()
+    {
+        yield return new WaitForSeconds(3f); // 3秒待つ
+        message.text = ""; // 表示テキストを消す
+    }
+
+    // 負けの処理
+    void Loss()
+    {
+        message.text = "GAME OVER";
+        finish = true; // 終了フラグをtrueに
+    }
+
+    //*勝ちの処理
+    void Win()
+    {
+        message.text = "WIN!!";
+        finish = true; //終了フラグをtrueに
+        if (High < Score)
+        {   // ハイスコアを超えていたら更新
+            High = Score;
+            PlayerPrefs.SetInt("high", High);
+            high.text = "Record: " + High;
+        }
     }
 }
--- Unity/Portfolio/MyProject/Assets/OtherData.cs
+++ Unity/Portfolio/MyProject/Assets/OtherData.cs
@@ -4,34 +4,129 @@ using UnityEngine;
 
 public class OtherData : MonoBehaviour
 {
-    int _value = 0;
-    public int value
+    // 敵キャラのパラメーター
+    Dictionary<string, int> param = new Dictionary<string, int>()
     {
-        get
+        {"power", 15}, // 体力
+        {"level", 1}, // レベル
+        {"exp", 0} // 経験値
+    };
+
+    // レベルごとのカラー
+    Color[] levelColor = {
+        new Color(0f, 0f, 0.75f),
+        new Color(0f, 0f, 1f),
+        new Color(0.25f, 0.25f, 1f),
+        new Color(0.5f, 0.5f, 1f),
+        new Color(0.75f, 0.75f, 1f),
+        new Color(1f, 1f, 1f)
+    };
+
+    float dv = 1f; // 移動係数
+
+    GameObject player = null; // プレイヤー
+    Main_ball_SceneScript sss = null; // メインプログラム
+
+    // 初期化処理
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        sss = player.GetComponent<Main_ball_SceneScript>();
+    }
+
+    // レベルを得る
+    public int Level()
+    {
+        return param["level"];
+    }
+
+    // 経験値を増やす
+    public void AddExp(int n)
+    {
+        param["exp"] += n;
+        if (param["exp"] >= 10)
         {
-            return _value;
-        }
-        set
-        {
-            _value = value;
+            // 10以上ならレベルアップ
+            levelUp();
         }
     }
-    Color _color = new Color(1f, 1f, 1f, 1f);
 
-    public Color Color
+    // レベルアップ処理
+    void levelUp()
     {
-        get
+        if (param["level"] == 5)
         {
-            return _color;
+            return; // 最大5まで
         }
-        set
-        {
-            _color = value;
+        param["level"]++; // レベルを上げる
+        param["exp"] = 0; // 経験値をゼロに
+        var r = gameObject.GetComponent<Renderer>();
+        r.material.color = levelColor[param["level"]];
+        dv = param["level"]; // 移動係数を設定
+    }
+
+    // 戦闘
+    void Fight()
+    {
+        var pr = player.GetComponent<Rigidbody>();
+        var mr = GetComponent<Rigidbody>();
+        // magnitudeを比べて価値を判定
+        if (pr.velocity.magnitude > mr.velocity.magnitude)
+        {   //負けた時の判定
+            Debug.Log(gameObject.name + ": Loss...");
+            param["power"] -= sss.Level(); // 体力低下
+            AddExp(sss.Level() / 2); // 経験値追加
+            if (param["power"] <= 0)
+            {   // 体力ゼロで消滅する
+                sss.Flag(this.gameObject.name);
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {   // 勝った時の処理
+            Debug.Log(gameObject.name + ": Gotcha!");
+            AddExp(sss.Level()); // 経験値追加
         }
     }
 
-    public void AddValue()
+    // 表示の更新
+    void FixedUpdate()
     {
-        _value += 1;
+        if (sss.Finish())
+        {
+            return; // 終了時は抜ける
+        }
+        var dp = player.transform.position - transform.position;
+        var r = GetComponent<Rigidbody>();
+        r.AddForce(dp / 10 * dv);
+    }
+
+    // 衝突時の処理
+    void OnCollisionEnter(Collision collision)
+    {
+        if (sss.Finish())
+        {
+            return; // 終了時は実行しない
+        }
+        if (collision.gameObject.name == "Player")
+        {   // プレイヤーと衝突した時
+            var halo = (Behaviour)GetComponent("Halo");
+            halo.enabled = false;
+            Fight();
+        }
+    }
+
+    // 衝突終了時の処理
+    void OnCollisionExit(Collision collision)
+    {
+        if (sss.Finish())
+        {
+            return; // 終了時は実行しない
+        }
+        if (collision.gameObject.name == "player")
+        {   // プレイヤーから離れるとき
+            var halo = (Behaviour)GetComponent("Halo");
+            halo.enabled = false;
+        }
     }
 }
